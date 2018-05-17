48ed63cc4d89a35868977ab01fff9658ed3e98d1
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-05-17 02:27:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var stringifyFile;
 app.use(bodyParser.json()); 
 
 app.get('/getNote', function (req, res) {
-    //console.log('Otrzymałem żądanie GET do strony /list_user');
+    
     fs.readFile('./test.json', 'utf8', function(err, data) {
 		if (err) throw err;
 		stringifyFile = data
@@ -16,17 +16,14 @@ app.get('/getNote', function (req, res) {
 
 });
 
-//app.post('/updateNote/:note' function (req, res) {
 
 app.post('/updateNote/:note', function (req, res) {
-	//console.log('POST');
+	
 	stringifyFile = req.params.note;
 	fs.writeFile('./test.json', stringifyFile, function(err) {
 		if (err) throw err;
 		console.log('file updated');
-		//res.end();
 	});
 });
-
 
 app.listen(3000);
